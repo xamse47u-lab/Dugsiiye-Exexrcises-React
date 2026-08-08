a63fa67a065ab86c93857f9e576526c704cd6e0c
xamse47u-lab <xamse47u@gmail.com>
2026-08-08 11:43:39 +0300

    Add exercise 3

---
+++ exercise 3/src/App.jsx
@@ -0,0 +1,10 @@
+import UseCard from "./UseCard"
+
+const App = () => {
+  return (
+    <>
+      <UseCard name={"Dugsiiye"} email={"dugsiiye@545.com"} />
+    </>
+  );
+}
+export default App;
---
+++ exercise 3/src/UseCard.jsx
@@ -0,0 +1,9 @@
+const UseCard = ({ name, email }) => {
+  return (
+    <div>
+      <h2> Hello , your name : {name}</h2>
+      <p> {email}</p>
+    </div>
+  );
+};
+export default UseCard;
